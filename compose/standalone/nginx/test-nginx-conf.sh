2e65b50d0439faa30bba828abaa108909bf7bd8d
xfl12345 <xfl12345>
2025-10-30 13:00:43 +0800

    chore(nginx): 提高测试脚本排查能力

--- compose/standalone/nginx/test-nginx-conf.sh
+++ compose/standalone/nginx/test-nginx-conf.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 docker compose run --rm --name=tmp-test-nginx --pull=never \
   --volume='/var/run/foxy-dock/lab/test:/var/run/nginx' \
-  --entrypoint='/usr/bin/bash -c' nginx '/docker-entrypoint.d/05-just-init.sh && nginx -t'
+  --entrypoint='/usr/bin/bash -c' nginx '/docker-entrypoint.d/05-just-init.sh && nginx -T'
 if [ $? -eq 0 ]; then
   echo "Nginx configuration is valid"
 else
